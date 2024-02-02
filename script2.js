404b02e25e34f8f19482915c99d870332401be5f
kerophpkabe <157329630+kerophpkabe@users.noreply.github.com>
2024-02-02 18:23:21 +0900

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,45 @@
+'use strict'
+
+// // 増やすものを作成する
+// let fueruyouso = document.createElement('img');
+// // 増やす要素の中身を作成（htmlに記述される）
+// fueruyouso.src = 'img/sample1.png'
+// fueruyouso.width = 220;
+// fueruyouso.height = 220; //ここではpxの記述は不要
+
+// IDに要素を増やす
+function gebid() {
+    let fueruyouso = document.createElement('img');
+// 増やす要素の中身を作成（htmlに記述される）
+fueruyouso.src = 'img/sample1.png'
+fueruyouso.width = 220;
+fueruyouso.height = 220; //ここではpxの記述は不要
+    const iddefuyasu = document.getElementById('target')
+    iddefuyasu.appendChild(fueruyouso);
+}
+// IDの要素を消す
+function gebiddel() {
+    const iddekesu = document.getElementById('target')
+    // iddekesu.removeChild(iddekesu.firstElementChild);
+    iddekesu.querySelector("img").remove();
+}
+
+
+// Classに要素を増やす
+function gebcl() {
+    let fueruyouso = document.createElement('img');
+// 増やす要素の中身を作成（htmlに記述される）
+fueruyouso.src = 'img/sample1.png'
+fueruyouso.width = 220;
+fueruyouso.height = 220; //ここではpxの記述は不要
+    const classdefuyasu = document.getElementsByClassName('target')
+    classdefuyasu[0].appendChild(fueruyouso);
+}
+// Classの要素を消す
+function gebcldel() {
+    const classdekesu = document.getElementsByClassName('target')
+//    classdekesu[0].removeChild(classdekesu[0].firstElementChild);
+    classdekesu[0].querySelector("img").remove();
+}
+
+// 何もない空間に要素を増やす
